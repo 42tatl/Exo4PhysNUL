a4d5abc96af44e9ed14ee6f4acd2055c96638588
lilimle <lili.mouelle@epfl.ch>
2025-04-09 22:15:34 +0200

    jsp si c'est ca qu'on veut mais balec

--- questiona.py
+++ questiona.py
@@ -7,23 +7,22 @@ import functions as fct
 
 executable = './Exe'  # Remove .exe for Mac
 repertoire = r"/Users/lilimouelle/Desktop/PHYSNUM/Exo4PhysNUL"  # Modify for correct directory
+output_template = "output.out"
 os.chdir(repertoire)
 
 input_filename = "configa.in"
-output_template = "output_{paramstr}_{value}.out"
 
 params = fct.read_in_file(input_filename)
 R, r1, epsilon_a, epsilon_b, uniform_rho_case, VR, rho0, N1, N2 = fct.get_params(params)
 
-output_phi = fct.run_simulation(executable, input_filename,"N", [3], {"adapt": 0})
-r, phi = fct.read_output_file_phi(output_phi)
+output = fct.run_simulation(executable, input_filename,output_template) #je comprends pas comment je dois utiliser les fonctions lol
+r, phi = fct.read_output_file_phi("outputs/output.out_phi.out")
 
 
 plt.figure()
 plt.plot(r, phi, marker='o')
-plt.xlabel("r (m)")
-plt.ylabel("φ(r) (V)")
-plt.title("Potentiel φ en fonction de r")
+plt.xlabel(r'r [m]')
+plt.ylabel(r'$\phi$ [V]')
 plt.grid(True)
 plt.tight_layout()
 fct.save_figure("phi_vs_r.png")
